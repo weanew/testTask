53a7b34dea28eeb768bf7fb2d43680491eb2dad1
weanew <weanew@yandex.ru>
2021-02-09 14:07:58 +0300

    test for the generator from task 2 added

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from html_generator.generator import generator_task1
+from html_generator.generator import generator_task1, generator_task2
 
 
 class TestGenerator(unittest.TestCase):
@@ -8,17 +8,29 @@ class TestGenerator(unittest.TestCase):
         request = """
                 [
                    {
-                      "title":"Title #1",
-                      "body":"Hello, World 1!"
+                      "title": "Title #1",
+                      "body": "Hello, World 1!"
                    },
                    {
-                      "title":"Title #2",
-                      "body":"Hello, World 2!"
+                      "title": "Title #2",
+                      "body": "Hello, World 2!"
                    }
                 ]
                 """
         response = '<h1>Title #1</h1><p>Hello, World 1!</p><h1>Title #2</h1><p>Hello, World 2!</p>'
         self.assertEqual(generator_task1(request), response)
+
+    def test_generator_task2(self):
+        request = """
+                    [
+                       {
+                          "h3": "Title #1",
+                          "div": "Hello, World 1!"
+                       }
+                    ]
+                    """
+        response = '<h3>Title #1</h3><div>Hello, World 1!</div>'
+        self.assertEqual(generator_task2(request), response)
 
 
 if __name__ == '__main__':
