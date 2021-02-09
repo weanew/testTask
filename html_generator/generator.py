9b5f0c6a3ac1a22777e53f681a55fb20f671c0a6
weanew <weanew@yandex.ru>
2021-02-09 15:48:25 +0300

    Generator for task 3 added

--- html_generator/generator.py
+++ html_generator/generator.py
@@ -29,3 +29,19 @@ def generator_task2(request: str) -> str:
             output += ET.tostring(element).decode()
 
     return output
+
+
+def generator_task3(request: str) -> str:
+    request = json.loads(request)
+    output = ''
+
+    if type(request) is list:
+        ul = ET.Element('ul')
+        for item in request:
+            li = ET.SubElement(ul, 'li')
+            for key in item:
+                element = ET.SubElement(li, key)
+                element.text = item[key]
+        output += ET.tostring(ul).decode()
+
+    return output
