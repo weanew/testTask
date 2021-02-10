7f2a0240a60c3397870c0d7033b70f701abe2584
weanew <weanew@yandex.ru>
2021-02-11 00:55:16 +0300

    tests updated

--- tests.py
+++ tests.py
@@ -1,36 +1,8 @@
 import unittest
-from html_generator.generator import *
+from html_generator.generator import main_generator
 
 
 class TestGenerator(unittest.TestCase):
-
-    def test_generator_task1(self):
-        request = """
-                [
-                   {
-                      "title": "Title #1",
-                      "body": "Hello, World 1!"
-                   },
-                   {
-                      "title": "Title #2",
-                      "body": "Hello, World 2!"
-                   }
-                ]
-                """
-        response = '<h1>Title #1</h1><p>Hello, World 1!</p><h1>Title #2</h1><p>Hello, World 2!</p>'
-        self.assertEqual(generator_task1(request), response)
-
-    def test_generator_task2(self):
-        request = """
-                    [
-                       {
-                          "h3": "Title #1",
-                          "div": "Hello, World 1!"
-                       }
-                    ]
-                    """
-        response = '<h3>Title #1</h3><div>Hello, World 1!</div>'
-        self.assertEqual(generator_task2(request), response)
 
     def test_generator_task3(self):
         request = """
@@ -47,7 +19,7 @@ class TestGenerator(unittest.TestCase):
                     """
         response = '<ul><li><h3>Title #1</h3><div>Hello, World 1!</div></li><li><h3>Title #2</h3>' \
                    '<div>Hello, World 2!</div></li></ul>'
-        self.assertEqual(generator_task3(request), response)
+        self.assertEqual(main_generator(request), response)
 
     def test_generator_task4(self):
         request = """
@@ -56,7 +28,7 @@ class TestGenerator(unittest.TestCase):
                           "span": "Title #1",
                           "content": [
                             {
-                                "p": "example 1",
+                                "p": "Example 1",
                                 "header": "header 1"    
                             }
                           ]
@@ -66,18 +38,7 @@ class TestGenerator(unittest.TestCase):
                     """
         response = '<ul><li><span>Title #1</span><content><ul><li><p>Example 1</p><header>header 1</header></li></ul>' \
                    '</content></li><li><div>div 1</div></li></ul>'
-        self.assertEqual(generator_task4(request), response)
-
-    def test_generator_task5(self):
-        request = """
-                    {
-                       "p.my-class#my-id":"hello",
-                       "p.my-class1.my-class2":"example<a>asd</a>"
-                    }
-                    """
-        response = '<p class="my-class" id="my-id">hello</p><p class="my-class1 my-class2">example&lt;a&gt;asd&lt;' \
-                   '/a&gt;</p>'
-        self.assertEqual(generator_task5(request), response)
+        self.assertEqual(main_generator(request), response)
 
 
 if __name__ == '__main__':
