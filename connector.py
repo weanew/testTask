7459b7f4580e95a3bc3a7451654e58468ab55141
weanew <weanew@yandex.ru>
2021-02-11 00:46:47 +0300

    Connector added

--- connector.py
+++ connector.py
@@ -1,17 +1,38 @@
-from html_generator.generator import generator_task1
+import json
+import asyncio
+import websockets
 
-if __name__ == '__main__':
-    request = """
-                [
-                   {
-                      "title":"Title #1",
-                      "body":"Hello, World 1!"
-                   },
-                   {
-                      "title":"Title #2",
-                      "body":"Hello, World 2!"
-                   }
-                ]
-                """
-    res = generator_task1(request)
-    print(res)
+
+class Connector:
+
+    def __init__(self):
+        self.socket_session = None
+
+    async def connect(self, uri: str) -> "Connector":
+        self.socket_session = await websockets.connect(uri).__aenter__()
+        return self
+
+    async def disconect(self):
+        await self.socket_session.close()
+        self.socket_session = None
+
+    async def call(self, **kwargs) -> str:
+        msg = json.dumps(kwargs)
+        ws = self.socket_session
+        await ws.send(msg)
+
+        response = await ws.recv()
+        return response
+
+
+async def run_con(uri: str) -> str:
+    connector = Connector()
+    connector = await connector.connect(uri)
+    response = await connector.call(arg1=234, art2='abcd', arg3=5)
+    await connector.disconect()
+    print(response)
+    return response
+
+# if __name__ == '__main__':
+#     uri = 'ws://localhost:8765'
+#     asyncio.get_event_loop().run_until_complete(run_con(uri))
