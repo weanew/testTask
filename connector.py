1aa27118d4294fd1e20b1887afbf03800168db7f
weanew <weanew@yandex.ru>
2021-02-15 12:19:25 +0300

    connector modified. Added the ability to create a connection pool

--- connector.py
+++ connector.py
@@ -1,47 +1,84 @@
 import json
 import asyncio
 import websockets
+import time
+from pprint import pprint
 
 
 class Connector:
 
     def __init__(self):
-        self.socket_session = None
+        self.uri = None
+        self.__pool_min = 3
+        self.__pool_max = 10
+        self.__socket_pool = []
+
+    async def __connect_socket(self):
+        socket = await websockets.connect(self.uri)
+        socket_lst = [socket, 'ready']
+        self.__socket_pool.append(socket_lst)
+        return socket_lst
+
+    async def __get_ready_socket(self):
+        socket = None
+        for socket_lst in self.__socket_pool:
+            if socket_lst[1] == 'ready':
+                socket = socket_lst
+                return socket
+        if socket is None:
+            if len(self.__socket_pool) == self.__pool_max:
+                while socket is None:
+                    for socket_lst in self.__socket_pool:
+                         if socket_lst[1] == 'ready':
+                            socket = socket_lst
+                    await asyncio.sleep(1)
+                return socket
+            elif len(self.__socket_pool) < self.__pool_max:
+                socket_lst = await self.__connect_socket()
+                return socket_lst
 
     async def connect(self, uri: str) -> "Connector":
-        if self.socket_session is None:
-            self.socket_session = await websockets.connect(uri).__aenter__()
+        self.uri = uri
+        if len(self.__socket_pool) == 0:
+            await asyncio.gather(*[self.__connect_socket() for _ in range(0, self.__pool_min)])
         else:
             raise Exception("Connection already exists.")
         return self
 
-    async def disconect(self):
-        if self.socket_session is not None:
-            await self.socket_session.close()
-            self.socket_session = None
+    async def disconnect(self):
+        if len(self.__socket_pool) != 0:
+            for socket_tuple in self.__socket_pool:
+                await socket_tuple[0].close()
+            self.__socket_pool = []
         else:
             raise Exception("Connection does not exist.")
 
     async def call(self, **kwargs) -> str:
-        if self.socket_session is not None:
+        if len(self.__socket_pool) != 0:
             msg = json.dumps(kwargs)
-            ws = self.socket_session
-            await ws.send(msg)
-
-            response = await ws.recv()
+            socket_lst = await self.__get_ready_socket()
+            socket_lst[1] = 'busy'
+            await socket_lst[0].send(msg)
+            print('stop here')
+            response = await socket_lst[0].recv()
+            socket_lst[1] = 'ready'
             return response
         else:
             raise Exception("Connection does not exist.")
 
 
-async def run_con(uri: str) -> str:
+async def start_con(uri: str):
     connector = Connector()
     connector = await connector.connect(uri)
-    response = await connector.call(arg1=234, art2='abcd', arg3=5)
-    await connector.disconect()
-    print(response)
-    return response
+    results = await asyncio.gather(*[connector.call(arg1=234, art2='abcd', arg3=5) for _ in range(0, 1000)])
+    await connector.disconnect()
+    pprint(results)
+    return results
+
+
 
 # if __name__ == '__main__':
 #     uri = 'ws://localhost:8765'
-#     asyncio.get_event_loop().run_until_complete(run_con(uri))
+#     t0 = time.time()
+#     asyncio.get_event_loop().run_until_complete(start_con(uri))
+#     print(f'total_time: {time.time() - t0} sec')
